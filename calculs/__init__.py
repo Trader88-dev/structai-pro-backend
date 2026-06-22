cd1201ffa7c05c2fee72a5deee10f06e40151195
trader88 <82215090+Trader88-dev@users.noreply.github.com>
2026-06-22 18:59:51 +0200

    Update __init__.py

--- calculs/__init__.py
+++ calculs/__init__.py
@@ -1,17 +1,11 @@
-from .betons import BETONS
-from .aciers import ACIERS
-from .enrobages import ENROBAGES
-
+from .materiaux import BETONS, ACIERS, ENROBAGES
 from .poutre import EntreePoutre, calculer_poutre
 from .poteau import EntreePoteau, calculer_poteau
 from .semelle import EntreeSemelle, calculer_semelle
 from .radier import EntreeRadier, calculer_radier
 from .dalle import EntreeDalle, calculer_dalle
-
 from .poutre_continue import EntreePoutreContinue, Travee, calculer_poutre_continue
-
 from .voile import EntreeVoile, calculer_voile
-
 from .escalier import EntreeEscalier, calculer_escalier
 from .linteau import EntreeLinteau, calculer_linteau
 from .semelle_isolee import EntreeSemIsolee, calculer_semelle_isolee
